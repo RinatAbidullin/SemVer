4c6f2766314750f844768a9147d5ea1bdaacd51f
Rinat Abidullin <rinatabidullin@gmail.com>
2024-02-09 19:07:04 +0500

    Добавлен Privacy Manifest

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
     targets: [
         .target(
             name: "SemVer",
-            dependencies: []
+            dependencies: [],
+            resources: [.copy("PrivacyInfo.xcprivacy")]
         ),
         .testTarget(
             name: "SemVerTests",
